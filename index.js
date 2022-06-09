743a8e8466c367cb1206fecb32756efeb8022064
Anand4405 <anandpatil4405@gmail.com>
2022-06-09 16:24:57 +0530

    changed nodemon to node

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const env = require('./config/environment');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = process.env.PORT|| 8000;
+const port =  8000||process.env.PORT;
 require('./config/view-helpers')(app);
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
