e01f9e7896fa8193caa101ee4ac075f36b7a8b0f
Anand4405 <anandpatil4405@gmail.com>
2022-06-09 16:08:34 +0530

    some minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const env = require('./config/environment');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = process.env.PORT|| 8000;
 require('./config/view-helpers')(app);
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
